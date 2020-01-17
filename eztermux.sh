b5b6bff63b9e72e3527216ba0c90e32c2f33c144
Co-dex <1042220898@qq.com>
2020-01-17 21:20:01 +0800

    Update eztermux.sh

--- eztermux.sh
+++ eztermux.sh
@@ -4,7 +4,7 @@ echo "Backup '$PREFIX/etc/apt/sources.list' to '$PREFIX/etc/apt/sources.listsour
 echo
 cp "$PREFIX/etc/apt/sources.list" "$PREFIX/etc/apt/sources.list.bak"
 echo "change default repository to USTC repository"
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@' '$PREFIX/etc/apt/sources.list'
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
 echo "if you meet some error can reboot termux and execute pkg up or dpkg --configure -a"
 pkg up
 echo "Add more keys to touch Keyboard"
